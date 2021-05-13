3f016bb87e159a834cc462dfea036771fe993433
wzp123321 <wanzhipengx@163.com>
2021-05-13 20:56:27 +0800

    修改代码

--- new-component/src/components/tree/index.vue
+++ new-component/src/components/tree/index.vue
@@ -3,12 +3,12 @@
     <a-tree
       v-if="hasloaded"
       :tree-data="treeData"
-      checkable
+      :checkable="true"
       showIcon
       showLine
+      :load-data="onLoadData"
       @check="treeCheck"
       v-model="checkedKeys"
-      :load-data="onLoadData"
     >
       <a-icon
         slot="switcherIcon"
@@ -16,16 +16,17 @@
         :style="{ fontSize: '16px' }"
       />
     </a-tree>
-    <a-button type="primary" @click="submit"> 提交</a-button>
+    <a-button type="primary" @click="submitSingleArray"> 提交</a-button>
   </div>
 </template>
 <script>
-import { Tree, Icon } from "ant-design-vue";
+import { Tree, Icon, Button } from "ant-design-vue";
 import { treeData } from "./mock";
 export default {
   components: {
     "a-tree": Tree,
-    "a-icon": Icon
+    "a-icon": Icon,
+    "a-button": Button
   },
   data() {
     return {
@@ -34,70 +35,198 @@ export default {
         title: "name"
       },
       treeData,
-      checkedKeys: ["01010201", "01010101", "01010102", "010101"],
-      areaSchoolMap: new Map(),
-      hasloaded: false
+      checkedKeys: [],
+      hasloaded: false,
+      selectDataList: [] // 选中的数组
     };
   },
   async created() {
-    await this.initData();
+    await this.initSingleArrayData();
     this.hasloaded = true;
-    console.log(this.treeData);
   },
   methods: {
-    initData() {
-      const { checkedKeys, treeData } = this;
-      treeData.forEach((item, idx) => {
-        item.children.forEach((childItem, idx1) => {
-          childItem.children.forEach(async (sonItem, idx2) => {
-            // 如果有区
-            if (checkedKeys.includes(sonItem.key)) {
-              console.log(sonItem.key);
-              const res = await this.loadData(sonItem.key);
-              console.log(res);
-              this.$set(
-                this.treeData[idx].children[idx1].children[idx2],
-                "children",
-                res
-              );
+    /**
+     * 扁平化数组下回显
+     */
+    initSingleArrayData() {
+      const { treeData } = this;
+      // const data = [
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0101",
+      //     cityName: "省1市1",
+      //     areaId: "010101",
+      //     areaName: "省1市1区1",
+      //     schoolId: "111111",
+      //     schoolName: "111111"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0101",
+      //     cityName: "省1市1",
+      //     areaId: "010101",
+      //     areaName: "省1市1区1",
+      //     schoolId: "222222",
+      //     schoolName: "222222"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0101",
+      //     cityName: "省1市1",
+      //     areaId: "010101",
+      //     areaName: "省1市1区1",
+      //     schoolId: "333333",
+      //     schoolName: "333333"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0101",
+      //     cityName: "省1市1",
+      //     areaId: "010102",
+      //     areaName: "省1市1区2",
+      //     schoolId: "444444",
+      //     schoolName: "444444"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0101",
+      //     cityName: "省1市1",
+      //     areaId: "010102",
+      //     areaName: "省1市1区2",
+      //     schoolId: "555555",
+      //     schoolName: "555555"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0102",
+      //     cityName: "省1市2",
+      //     areaId: "010201",
+      //     areaName: "省1市2区1",
+      //     schoolId: "888888",
+      //     schoolName: "888888"
+      //   },
+      //   {
+      //     provinceId: "01",
+      //     provinceName: "省-1",
+      //     cityId: "0102",
+      //     cityName: "省1市2",
+      //     areaId: "010201",
+      //     areaName: "省1市2区1",
+      //     schoolId: "999999",
+      //     schoolName: "999999"
+      //   },
+      //   {
+      //     provinceId: "02",
+      //     provinceName: "省-2",
+      //     cityId: "0202",
+      //     cityName: "省2市2",
+      //     areaId: "020202",
+      //     areaName: "省2市2区2",
+      //     schoolId: "2121212121",
+      //     schoolName: "2121212121"
+      //   },
+      //   {
+      //     provinceId: "02",
+      //     provinceName: "省-2",
+      //     cityId: "0202",
+      //     cityName: "省2市2",
+      //     areaId: "020202",
+      //     areaName: "省2市2区2",
+      //     schoolId: "2222222222",
+      //     schoolName: "2222222222"
+      //   }
+      // ];
+      const data = [];
+      // checkedKeys
+      this.checkedKeys = data.map(item => {
+        return item.schoolId;
+      });
+      let areaIds = data.map(item => {
+        return item.areaId;
+      });
+      areaIds = Array.from(new Set(areaIds));
+      treeData.forEach(provinceItem => {
+        provinceItem.children.forEach(cityItem => {
+          cityItem.children.forEach(areaItem => {
+            if (areaIds.includes(areaItem.key) && !areaItem.children) {
+              areaItem.children = [
+                {
+                  isLeaf: true,
+                  key: `${areaItem.key}01`,
+                  title: "111111"
+                }
+              ];
             }
           });
         });
       });
     },
+    /**
+     * 多层级数组下回显
+     */
+    initMulitArrayData() {
+      const { selectDataList } = this;
+      selectDataList.forEach(provinceItem => {
+        provinceItem.children.forEach(cityItem => {
+          cityItem.children.forEach(areaItem => {
+            areaItem.children.forEach(schoolItem => {
+              this.checkedKeys.push(schoolItem.key);
+            });
+            /**
+             * 根据areaItem.key调用接口拼接数据
+             */
+          });
+        });
+      });
+    },
     treeCheck(checkkeys, e) {
-      console.log(checkkeys);
       this.checkkeys = checkkeys;
     },
+    /**
+     * tree 加载数据
+     */
     async onLoadData(node) {
-      console.log(node, node.dataRef);
-
-      if (
-        node.dataRef.hasOwnProperty("first") &&
-        node.dataRef.hasOwnProperty("second") &&
-        node.dataRef.hasOwnProperty("third") &&
-        !this.treeData[node.dataRef.first].children[
-          node.dataRef.second
-        ].children[node.dataRef.third].hasOwnProperty("children")
-      ) {
-        const res = await this.loadData(node.dataRef.key);
-        this.$set(
-          this.treeData[node.dataRef.first].children[node.dataRef.second]
-            .children[node.dataRef.third],
-          "children",
-          res
-        );
-        res.forEach(item => {
-          this.areaSchoolMap.set(item.key, node.dataRef.key);
+      const { treeData } = this;
+      if (node.dataRef.type === "area") {
+        treeData.forEach((provinceItem, provinceIndex) => {
+          provinceItem.children.forEach((cityItem, cityIndex) => {
+            cityItem.children.forEach(async (areaItem, areaIndex) => {
+              if (node.dataRef.key === areaItem.key) {
+                try {
+                  const res = await this.loadData(
+                    node.dataRef.key,
+                    `${provinceItem.title}_${cityItem.title}_${areaItem.title}`
+                  );
+                  this.$set(
+                    this.treeData[provinceIndex].children[cityIndex].children[
+                      areaIndex
+                    ],
+                    "children",
+                    res
+                  );
+                } catch (error) {
+                  console.log(error);
+                }
+              }
+            });
+          });
         });
-        console.log(this.treeData);
       }
     },
-    loadData(parentKey) {
+    /**
+     * 模拟请求数据
+     */
+    loadData(parentKey, title) {
       return new Promise((resolve, reject) => {
         resolve([
           {
-            title: "测试学校1",
+            title: `${title}测试学校1`,
             key: `${parentKey}01`,
             children: [],
             checkable: false,
@@ -105,7 +234,7 @@ export default {
             disableCheckbox: false
           },
           {
-            title: "测试学校2",
+            title: `${title}测试学校1`,
             key: `${parentKey}02`,
             children: [],
             checkable: false,
@@ -115,11 +244,209 @@ export default {
         ]);
       });
     },
+    getAllProvinceIds(provinceId) {
+      let ids = [];
+      const { treeData } = this;
+      treeData.forEach(provinceItem => {
+        if (provinceItem.key == provinceId) {
+          ids.push(provinceItem.key);
+          // 市
+          provinceItem.children.forEach(cityItem => {
+            ids.push(cityItem.key);
+            cityItem.children.forEach(areaItem => {
+              ids.push(areaItem.key);
+              areaItem.children.forEach(schoolItem => {
+                ids.push(schoolItem.key);
+              });
+            });
+          });
+        }
+      });
+      return ids;
+    },
+    getAllCityIds(provinceId, cityId) {
+      let ids = [];
+      const { treeData } = this;
+      treeData.forEach(provinceItem => {
+        if (provinceItem.key == provinceId) {
+          // 市
+          provinceItem.children.forEach(cityItem => {
+            if (cityItem.key === cityId) {
+              ids.push(cityItem.key);
+              cityItem.children.forEach(areaItem => {
+                ids.push(areaItem.key);
+                areaItem.children.forEach(schoolItem => {
+                  ids.push(schoolItem.key);
+                });
+              });
+            }
+          });
+        }
+      });
+      return ids;
+    },
+    getAllAeaIds(provinceId, cityId, areaId) {
+      let ids = [];
+      const { treeData } = this;
+      treeData.forEach(provinceItem => {
+        if (provinceItem.key == provinceId) {
+          provinceItem.children.forEach(cityItem => {
+            if (cityId == cityItem.key) {
+              cityItem.children.forEach(areaItem => {
+                if (areaItem.key == areaId) {
+                  ids.push(areaId);
+                  areaItem.children.forEach(schoolItem => {
+                    ids.push(schoolItem.key);
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+      return ids;
+    },
+    submitSingleArray() {
+      this.selectDataList = [];
+      const { checkedKeys, treeData } = this;
+      treeData.forEach((provinceItem, provinceIndex) => {
+        provinceItem.children.forEach((cityItem, cityIndex) => {
+          cityItem.children.forEach(async (areaItem, areaIndex) => {
+            if (!areaItem.children) {
+              try {
+                const res = await this.loadData(
+                  areaItem.key,
+                  `${provinceItem.title}_${cityItem.title}_${areaItem.title}`
+                );
+                this.$set(
+                  this.treeData[provinceIndex].children[cityIndex].children[
+                    areaIndex
+                  ],
+                  "children",
+                  res
+                );
+                res.forEach(schoolItem => {
+                  this.selectDataList.push({
+                    provinceId: provinceItem.key,
+                    provinceName: provinceItem.title,
+                    cityId: cityItem.key,
+                    cityName: cityItem.title,
+                    areaId: areaItem.key,
+                    areaName: areaItem.title,
+                    schoolId: schoolItem.key,
+                    schoolName: schoolItem.title
+                  });
+                });
+              } catch (error) {
+                console.log(error);
+              }
+            } else {
+              areaItem.children.forEach(schoolItem => {
+                if (checkedKeys.includes(schoolItem.key)) {
+                  this.selectDataList.push({
+                    provinceId: provinceItem.key,
+                    provinceName: provinceItem.title,
+                    cityId: cityItem.key,
+                    cityName: cityItem.title,
+                    areaId: areaItem.key,
+                    areaName: areaItem.title,
+                    schoolId: schoolItem.key,
+                    schoolName: schoolItem.title
+                  });
+                }
+              });
+            }
+          });
+        });
+      });
+      console.log(JSON.stringify(this.selectDataList));
+    },
+
     /**
-     * 提交
+     * 提交---------------------------数据结构为多级数组
      */
-    submit() {
-      const { checkkeys, treeData } = this;
+    submitMulitArray() {
+      const { checkedKeys, treeData } = this;
+      let checkId = JSON.parse(JSON.stringify(checkedKeys));
+      this.selectDataList = JSON.parse(JSON.stringify(treeData));
+      this.selectDataList.forEach(provinceItem => {
+        /**
+         * 如果当前省被选中
+         */
+        if (checkId.includes(provinceItem.key)) {
+          // 过滤掉当前省下所有id
+          const ids = this.getAllProvinceIds(provinceItem.key);
+          checkId = checkId.filter(idItem => {
+            return !ids.includes(idItem);
+          });
+        } else {
+          this.$set(provinceItem, "allSelected", false);
+          // 省下面的市遍历
+          provinceItem.children.forEach(cityItem => {
+            /**
+             * 省下面的市被选中
+             */
+            if (checkId.includes(cityItem.key)) {
+              const ids = this.getAllCityIds(provinceItem.key, cityItem.key);
+              console.log(ids);
+              checkId = checkId.filter(idItem => {
+                return !ids.includes(idItem);
+              });
+              console.log("checkId--------------", checkId);
+            } else {
+              /**
+               * 省下面的市没有被选中
+               */
+              this.$set(cityItem, "allSelected", false);
+              // 省下面的市没有被选中  -判断市下面的区
+              cityItem.children.forEach(areaItem => {
+                if (checkId.includes(areaItem.key)) {
+                  const ids = this.getAllCityIds(
+                    provinceItem.key,
+                    cityItem.key,
+                    areaItem.key
+                  );
+                  console.log(ids);
+                  checkId = checkId.filter(idItem => {
+                    return !ids.includes(idItem);
+                  });
+                } else {
+                  this.$set(areaItem, "allSelected", false);
+                  // 过滤掉没有被选中的学校
+                  areaItem.children =
+                    areaItem.children.length > 0
+                      ? areaItem.children.filter(schoolItem => {
+                          return checkId.includes(schoolItem.key);
+                        })
+                      : [];
+                  /**
+                   * 如果当前区下面学校都没有被选中  删掉当前区
+                   */
+                  if (areaItem.children.length === 0) {
+                    cityItem.children = cityItem.children.filter(reduceItem => {
+                      return reduceItem.key !== areaItem.key;
+                    });
+                    if (cityItem.children.length === 0) {
+                      provinceItem.children = provinceItem.children.filter(
+                        reduceItem => {
+                          return reduceItem.key !== cityItem.key;
+                        }
+                      );
+                      if (provinceItem.children.length === 0) {
+                        this.selectDataList = this.selectDataList.filter(
+                          reduceItem => {
+                            return reduceItem.key !== provinceItem.key;
+                          }
+                        );
+                      }
+                    }
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
     }
   }
 };
--- new-component/src/components/tree/mock.js
+++ new-component/src/components/tree/mock.js
@@ -1,141 +1,123 @@
 export const treeData = [{
     title: "省-1",
     key: "01",
-    checkable: false,
-    disableCheckbox: true,
+    index: 0,
+    type: 'province',
+    checkable: true,
+    disableCheckbox: false,
     children: [{
         title: "省1市1",
         key: "0101",
-        first: 0,
-        second: 0,
+        type: 'city',
+        index: 0,
         checkable: true,
         disableCheckbox: false,
         children: [{
             title: "省1市1区1",
             key: "010101",
-            first: 0,
-            second: 0,
-            third: 0,
+            type: 'area',
+            index: 0,
             checkable: true,
             disableCheckbox: false,
-            children: []
           },
           {
             title: "省1市1区2",
             key: "010102",
-            first: 0,
-            second: 0,
-            third: 1,
+            type: 'area',
+            index: 1,
             checkable: true,
             disableCheckbox: false,
-            children: []
           }
         ]
       },
       {
         title: "省1市2",
         key: "0102",
-        first: 1,
-        second: 1,
+        type: 'city',
+        index: 1,
         checkable: true,
         disableCheckbox: false,
         children: [{
             title: "省1市2区1",
             key: "010201",
-            first: 1,
-            second: 0,
-            third: 0,
+            type: 'area',
+            index: 0,
             checkable: true,
             disableCheckbox: false,
-            children: []
           },
           {
             title: "省1市2区2",
             key: "010202",
-            first: 1,
-            second: 0,
-            third: 1,
+            type: 'area',
+            index: 1,
             checkable: true,
             disableCheckbox: false,
-            children: []
           }
         ]
       }
     ],
-    checkable: false,
+    checkable: true,
   },
   {
     title: "省-2",
     key: "02",
-    first: 0,
-    checkable: false,
-    disableCheckbox: true,
+    type: 'province',
+    index: 1,
+    checkable: true,
+    disableCheckbox: false,
     children: [{
         title: "省2市1",
         key: "0201",
-        first: 0,
-        second: 0,
+        type: 'city',
+        index: 0,
         checkable: true,
         disableCheckbox: false,
         children: [{
             title: "省2市1区1",
             key: "020101",
-            first: 0,
-            second: 0,
+            type: 'area',
+            index: 0,
             checkable: true,
             disableCheckbox: false,
-            children: []
           },
           {
             title: "省2市1区2",
             key: "020102",
-            first: 0,
-            second: 0,
+            type: 'area',
+            index: 1,
             checkable: true,
             disableCheckbox: false,
-            children: []
           }
         ]
       },
       {
         title: "省2市2",
         key: "0202",
-        first: 1,
-        second: 1,
+        type: 'city',
+        index: 1,
         checkable: true,
         disableCheckbox: false,
         children: [{
             title: "省2市2区1",
             key: "020201",
-            first: 0,
-            second: 0,
+            type: 'area',
+            index: 0,
             checkable: true,
             disableCheckbox: false,
-            children: []
           },
           {
             title: "省2市2区2",
             key: "020202",
-            first: 0,
-            second: 0,
+            type: 'area',
+            index: 1,
             checkable: true,
             disableCheckbox: false,
-            children: []
           }
         ]
       }
     ],
-    checkable: false,
+    checkable: true,
   }
 ]
 
 
-data = [
-  {
-    type:'area',
-    id:'01',
-    schoolList:[
-
-    ]
-  }
-]
